b6082e5e78ae24b31c375800727c2e64ed76cf05
Your Name <you@example.com>
2020-05-24 17:25:56 +0300

    add standartization function

--- main.py
+++ main.py
@@ -26,16 +26,27 @@ for line in dataset:
         test_set_predictions = np.append(test_set_predictions, [temp_y])
     counter += 1
 
+
+def standardization(matrix):
+    matrix_std = np.copy(matrix)
+    for i in range(0, len(matrix_std[0])):
+        matrix_std[:, i] = (matrix[:, i] - matrix[:, i].mean()) / matrix[:, i].std()
+    return matrix_std
+
+
+train_set_features = standardization(train_set_features)
+test_set_features = standardization(test_set_features)
+
 w = np.zeros(1 + train_set_features.shape[1])
-eta = 0.00001
+eta = 0.0001
 costs = np.array([])
 
-
-for i in range(30):
-    output = np.dot(train_set_features, w[1:]) + w[0]
-    errors = (train_set_predictions - output)
-    w[1:] += + eta * np.dot(train_set_features.T, errors)
+for iteration in range(10):
+    outputs = np.dot(train_set_features, w[1:]) + w[0]
+    errors = (train_set_predictions - outputs)
+    w[1:] += eta * np.dot(train_set_features.T, errors)
     w[0] += eta * errors.sum()
-    print(w[0])
     # cost = (errors ** 2).sum() / 2.0
     # costs = np.append(costs, [cost])
+
+print(w)
